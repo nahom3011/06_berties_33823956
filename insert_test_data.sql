411ed0e4fa4e09d715de1af346808acc8a728937
Nbekele <nbeke001@gold.ac.uk>
2025-11-21 00:10:11 +0000

    gold smith

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, first, last, email, hashedPassword) VALUES
+  ('gold', 'Gold', 'Smiths', 'gold@gold.com', '$2b$10$/KZPUQ.nV0bvuwNbqC.M2OJT4a1Er0wkoGhLZYlBr8pXvFAWpLHcC');
